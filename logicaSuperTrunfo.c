cc8f67fe983318b174e41052c14028eb3763944b
Kaio Lyan <85936598+kaio-dot@users.noreply.github.com>
2024-10-18 14:53:00 +0000

    Primeiro Commit da minha vida haha

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,43 +1,145 @@
 #include <stdio.h>
-
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
+#include <locale.h>
+#include <string.h>
 
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
+    setlocale(LC_ALL, "");
 
-    
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+    char estadoA[100], estadoB[100];
+    char codigoEstadoA[100], codigoEstadoB[100];
+    char nomeCidadeA[100], nomeCidadeB[100];
+    int populacaoA, populacaoB, pontoTuristicoA, pontoTuristicoB;
+    float areaA, areaB, pibA, pibB;
+    char escolha;
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+    do {
+        // Entrada da primeira cidade
+        printf("=== Cadastro da Cidade 1 ===\n");
+        printf("Nome do Estado: ");
+        fgets(estadoA, sizeof(estadoA), stdin);
+        estadoA[strcspn(estadoA, "\n")] = '\0';
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+        printf("O código do estado: ");
+        fgets(codigoEstadoA, sizeof(codigoEstadoA), stdin);
+        codigoEstadoA[strcspn(codigoEstadoA, "\n")] = '\0';
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
+        printf("O nome da cidade: ");
+        fgets(nomeCidadeA, sizeof(nomeCidadeA), stdin);
+        nomeCidadeA[strcspn(nomeCidadeA, "\n")] = '\0';
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
+        printf("A população total: ");
+        scanf("%d", &populacaoA);
+        while (getchar() != '\n');
 
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+        printf("A quantidade de pontos turísticos: ");
+        scanf("%d", &pontoTuristicoA);
+        while (getchar() != '\n');
 
+        printf("A área da cidade (em km²): ");
+        scanf("%f", &areaA);
+        while (getchar() != '\n');
+
+        printf("O PIB (em milhões): ");
+        scanf("%f", &pibA);
+        while (getchar() != '\n');
+
+        // Entrada da segunda cidade
+        printf("\n=== Cadastro da Cidade 2 ===\n");
+        printf("Nome do Estado: ");
+        fgets(estadoB, sizeof(estadoB), stdin);
+        estadoB[strcspn(estadoB, "\n")] = '\0';
+
+        printf("O código do estado: ");
+        fgets(codigoEstadoB, sizeof(codigoEstadoB), stdin);
+        codigoEstadoB[strcspn(codigoEstadoB, "\n")] = '\0';
+
+        printf("O nome da cidade: ");
+        fgets(nomeCidadeB, sizeof(nomeCidadeB), stdin);
+        nomeCidadeB[strcspn(nomeCidadeB, "\n")] = '\0';
+
+        printf("A população total: ");
+        scanf("%d", &populacaoB);
+        while (getchar() != '\n');
+
+        printf("A quantidade de pontos turísticos: ");
+        scanf("%d", &pontoTuristicoB);
+        while (getchar() != '\n');
+
+        printf("A área da cidade (em km²): ");
+        scanf("%f", &areaB);
+        while (getchar() != '\n');
+
+        printf("O PIB (em milhões): ");
+        scanf("%f", &pibB);
+        while (getchar() != '\n');
+
+        // Comparação dos atributos
+        printf("\n--- Comparação de Cidades ---\n");
+
+        if (populacaoA > populacaoB) {
+            printf("Cidade 1 (%s) tem maior população.\n", nomeCidadeA);
+        } else if (populacaoA < populacaoB) {
+            printf("Cidade 2 (%s) tem maior população.\n", nomeCidadeB);
+        } else {
+            printf("Ambas as cidades têm a mesma população.\n");
+        }
+
+        if (areaA > areaB) {
+            printf("Cidade 1 (%s) tem maior área.\n", nomeCidadeA);
+        } else if (areaA < areaB) {
+            printf("Cidade 2 (%s) tem maior área.\n", nomeCidadeB);
+        } else {
+            printf("Ambas as cidades têm a mesma área.\n");
+        }
+
+        if (pibA > pibB) {
+            printf("Cidade 1 (%s) tem maior PIB.\n", nomeCidadeA);
+        } else if (pibA < pibB) {
+            printf("Cidade 2 (%s) tem maior PIB.\n", nomeCidadeB);
+        } else {
+            printf("Ambas as cidades têm o mesmo PIB.\n");
+        }
+
+        // Determinar a cidade vencedora
+        int pontosCidadeA = 0, pontosCidadeB = 0;
+
+        // População
+        if (populacaoA > populacaoB) {
+            pontosCidadeA++;
+        } else if (populacaoA < populacaoB) {
+            pontosCidadeB++;
+        }
+
+        // Área
+        if (areaA > areaB) {
+            pontosCidadeA++;
+        } else if (areaA < areaB) {
+            pontosCidadeB++;
+        }
+
+        // PIB
+        if (pibA > pibB) {
+            pontosCidadeA++;
+        } else if (pibA < pibB) {
+            pontosCidadeB++;
+        }
+
+        // Exibir o vencedor
+        if (pontosCidadeA > pontosCidadeB) {
+            printf("\nA cidade vencedora é: %s\n", nomeCidadeA);
+        } else if (pontosCidadeB > pontosCidadeA) {
+            printf("\nA cidade vencedora é: %s\n", nomeCidadeB);
+        } else {
+            printf("\nNenhuma cidade venceu, houve um empate.\n");
+        }
+
+        // Perguntar se deseja realizar outra comparação
+        printf("\nDeseja comparar outras cidades? (S/N): ");
+        scanf("%c", &escolha);
+        while (getchar() != '\n'); // Limpar o buffer
+
+    } while (escolha == 'S' || escolha == 's');
+
+    printf("Encerrando o programa...\n");
     return 0;
 }
